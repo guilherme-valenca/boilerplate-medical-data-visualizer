49661964102e442187b17d2704eef37ad68c25c6
guilherme-valenca <guilherme.valenca@sou.inteli.edu.br>
2025-09-04 11:58:47 +0000

    feat: tasks 1 to 5

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,18 +4,36 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv('medical_examination.csv')
 
 # 2
-df['overweight'] = None
+df['bmi'] = df['weight'] / (df['height'] ** 2)
+df['overweight'] = 1 if df['bmi'] > 25 else 0
 
 # 3
-
+    # 0 good / 1 bad
+df['cholesterol'] = 0 if df['cholesterol'] == 1 else 1
+df['gluc'] = 0 if df['gluc'] == 1 else 1
 
 # 4
 def draw_cat_plot():
+
+    # Creates an image with one graph (subplot)
+    fig, ax = plt.subplots(figsize=(10, 6))
+
+
+    sns.barplot(data=df, x='category', y='value', 
+                
+                hue='group', # Creates grouped/clustered bars based on the 'group' column
+                ax=ax #Tells seaborn to draw on the specific axes object created above
+                )
+
     # 5
-    df_cat = None
+    df_cat = pd.melt(df, 
+                    value_vars=['cholesterol', 'gluc', 'smoke', 'alco',
+                    'active', 'overweight'],
+                    value_name='variables'
+                    )
 
 
     # 6
